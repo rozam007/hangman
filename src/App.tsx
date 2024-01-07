1b6f13bad3ccbe78d66c14a4f1e8227b910d1f6c
rozam007 <rozam0040@gmail.com>
2024-01-07 15:15:10 +0500

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,40 @@
-import { KeyboardEvent, useEffect, useState, useCallback } from "react";
+import { useCallback, useEffect, useState } from "react";
+import { HangmanDrawing } from "./HangmanDrawing";
+import { HangmanWord } from "./HangmanWord";
+import { Keyboard } from "./Keyboard";
 import words from "./wordList.json";
-import { HangmanDrawing } from "./HangmanDrawing.tsx";
-import { HangmanWord } from "./HangmanWord.tsx";
-import { Keyboard } from "./Keyboard.tsx";
 
 function getWord() {
   return words[Math.floor(Math.random() * words.length)];
 }
 
 function App() {
-  const [wordToGuess, setWordToGuess] = useState(() => {
-    return getWord();
-  });
-
+  const [wordToGuess, setWordToGuess] = useState(getWord);
   const [guessedLetters, setGuessedLetters] = useState<string[]>([]);
 
   const incorrectLetters = guessedLetters.filter((letter) => !wordToGuess.includes(letter));
 
-  const isLoser = incorrectLetters.length > 6;
+  const isLoser = incorrectLetters.length >= 6;
   const isWinner = wordToGuess.split("").every((letter) => guessedLetters.includes(letter));
 
   const addGuessedLetter = useCallback(
     (letter: string) => {
       if (guessedLetters.includes(letter) || isLoser || isWinner) return;
-      setGuessedLetters((curr) => [...curr, letter]);
+
+      setGuessedLetters((currentLetters) => [...currentLetters, letter]);
     },
-    [guessedLetters]
+    [guessedLetters, isWinner, isLoser]
   );
 
-  const handler = (e: KeyboardEvent) => {
-    const key = e.key;
-    if (!key.match(/^[a-z]$/)) return;
-
-    e.preventDefault();
-    addGuessedLetter(key);
-  };
-
   useEffect(() => {
+    const handler = (e: KeyboardEvent) => {
+      const key = e.key;
+      if (!key.match(/^[a-z]$/)) return;
+
+      e.preventDefault();
+      addGuessedLetter(key);
+    };
+
     document.addEventListener("keypress", handler);
 
     return () => {
@@ -59,7 +57,7 @@ function App() {
     return () => {
       document.removeEventListener("keypress", handler);
     };
-  }, [guessedLetters]);
+  }, []);
 
   return (
     <div style={{ maxWidth: "800px", display: "flex", flexDirection: "column", alignItems: "center", gap: "2rem", margin: "auto" }}>
